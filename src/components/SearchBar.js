e3d8abd5b0a3ee09ce94fe9ffdf929733f8bfcaa
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-01 13:57:58 -0700

    added component SearchBar and App

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
