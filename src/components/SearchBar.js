9c4d31e88a903843c13d4b88aebc763aba56cb8b
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-02 22:41:25 -0700

    refactored App component to invoke callbacks in children(SearchBar) component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = {};
+  state = { term: "" };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
         </form>
       </div>
     );
