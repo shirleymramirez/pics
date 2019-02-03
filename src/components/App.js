406e9af135ae1715b513a1ac4f5d5d3db3cda001
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-02 23:07:36 -0700

    making api request using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {});
   }
   render() {
     return (
