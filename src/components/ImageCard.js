36b6946fb2514d65eb04d03138515411a0165610
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-05 19:31:07 -0700

    used callbacks on image load, and used ref's for DOM access

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
   componentDidMount() {
@@ -11,13 +11,17 @@ class ImageCard extends React.Component {
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
