fd43dfa88dc4757d25745dd6e6949ca8fd88f238
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-05 19:18:12 -0700

    added component ImageCard, ImageList.css and modified ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
-  const images = props.images.map(({ id, urls, description }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
